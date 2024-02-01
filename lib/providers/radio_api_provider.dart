09b51760177821867a6cabb11b8a97791091fa05
Oliana Cintas <oliana.cintas@gmail.com>
2024-02-01 02:31:34 +0100

    HTTPS filter

--- lib/providers/radio_api_provider.dart
+++ lib/providers/radio_api_provider.dart
@@ -5,12 +5,14 @@ import 'package:radio_oliana/models/radio_station.dart';
 
 Future<List<RadioStation>> getRadios() async {
   List<RadioStation> radiosStations = [];
+  RadioStation radioStation;
   var url = Uri.parse(
       "http://de1.api.radio-browser.info/json/stations/bystate/barcelona");
   Response response = await http.get(url);
   var json = jsonDecode(response.body);
   for (var radio in json) {
-    radiosStations.add(RadioStation.fromMap(radio));
+    radioStation = RadioStation.fromMap(radio);
+    if (radioStation.url.startsWith('https')) radiosStations.add(radioStation);
   }
   return radiosStations;
 }
